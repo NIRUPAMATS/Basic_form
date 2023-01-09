730daa524364b5d52d311039716462ae3e013e50
Devika S kumar <devikaskumar@Devikas-MacBook-Air.local>
2023-01-10 01:36:06 +0530

    Status

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Status from './Pages/Status/Status';
 import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 //import './index.css';
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Router>
-    <App />
+    <Status/>
     </Router>
   </React.StrictMode>
 );
