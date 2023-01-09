0f7e08052dc22c24f1595eadf33d194e141731cf
KeerthiAnilNair <keerthianilnair@gmail.com>
2023-01-10 01:01:44 +0530

    Review

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Review from './Pages/Review/Review';
 //import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Router>
-    <App />
+    <Review />
     </Router>
   </React.StrictMode>
 );
