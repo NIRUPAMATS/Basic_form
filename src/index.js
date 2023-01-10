d3f7626710ad6de3a9edd5affec31b1c6afe768c
Nadeem740 <nadeemblayparambil@gmail.com>
2023-01-10 11:18:40 +0530

    h

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import History from './Pages/History/History'
-import Dashboard from './Pages/Dashboard/Dashboard'
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-=======
-import Status from './Pages/Status/Status';
->>>>>>> 6745f0305bd32abfdb29bbf25ed7a8a9ba682fbc
-import { BrowserRouter as Router} from 'react-router-dom';
-<<<<<<< HEAD
-
-=======
 import reportWebVitals from './reportWebVitals';
-import Review from './Pages/Review/Review';
->>>>>>> d7d566d80938557ca278d0cd3d99335d621d75cf
-//import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-<<<<<<< HEAD
+  <React.StrictMode>
     <App />
-  </Router>
-=======
-<<<<<<< HEAD
-    <Dashboard />
-    {/* <History /> */}
-=======
-<<<<<<< HEAD
-    <Status/>
-=======
-    <Review />
->>>>>>> refs/remotes/origin/main
->>>>>>> 6745f0305bd32abfdb29bbf25ed7a8a9ba682fbc
-    </Router>
   </React.StrictMode>
->>>>>>> d7d566d80938557ca278d0cd3d99335d621d75cf
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
