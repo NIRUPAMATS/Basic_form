6b56c0519c1de5e8e7a0f5f4b8e126fe736a2965
Sneha Sani <snehasanip@gmail.com>
2023-01-10 08:16:20 +0530

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import History from './Pages/History/History'
+import Dashboard from './Pages/Dashboard/Dashboard'
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Router>
-    <App />
+    <Dashboard />
+    {/* <History /> */}
     </Router>
   </React.StrictMode>
 );
